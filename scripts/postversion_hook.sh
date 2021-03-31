382d82c721a7063112064914b1ac6d3e6e28b18a
fiuzagr <fiuzagr@gmail.com>
2021-03-31 10:48:59 -0300

    Update package.json & scripts

--- scripts/postversion_hook.sh
+++ scripts/postversion_hook.sh
@@ -2,5 +2,10 @@
 
 postversion_hook() {
   git push --follow-tags origin release
+  git checkout main
+  git pull
+  git checkout origin/release -- package.json
+  git commit -am "Update version in package.json"
+  git push origin main
 }
 postversion_hook
--- scripts/preversion_hook.sh
+++ scripts/preversion_hook.sh
@@ -4,9 +4,9 @@ preversion_hook() {
   git fetch --all
   git checkout release
   git pull
-  git checkout main -- package.json package-lock.json scripts
+  git checkout origin/main -- package.json package-lock.json scripts
   git commit -am "Update package.json & scripts"
-  git checkout main -- assets properties config.json index.js
+  git checkout origin/main -- assets properties config.json index.js
   git reset HEAD .
 }
 preversion_hook
