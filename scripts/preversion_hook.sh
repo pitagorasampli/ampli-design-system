33bf872521d56e9094f4b67cf1864c3aa6375bff
fiuzagr <fiuzagr@gmail.com>
2021-03-31 11:41:40 -0300

    download zeroheight tokens

--- scripts/preversion_hook.sh
+++ scripts/preversion_hook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+ZEROHEIGHT_TOKENS_FILENAME=zeroheight.json
+ZEROHEIGHT_TOKENS_URL=https://krotonds.zeroheight.com/api/token_file/70ccb45ceb73/share
+
 preversion_hook() {
   git fetch --all
   git checkout release
@@ -8,5 +11,6 @@ preversion_hook() {
   git commit -am "Update package.json & scripts"
   git checkout origin/main -- assets properties config.json index.js
   git reset HEAD .
+  wget -O properties/$ZEROHEIGHT_TOKENS_FILENAME $ZEROHEIGHT_TOKENS_URL
 }
 preversion_hook
