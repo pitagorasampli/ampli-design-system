98c949d008e95ee2f6a3397d3cdb9722f695a498
fiuzagr <fiuzagr@gmail.com>
2021-11-30 16:20:24 -0300

    Remove unused code

--- scripts/downloadtokens_hook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-ZEROHEIGHT_TOKENS_FILENAME=zeroheight.json
-
-
-downloadtokens_hook() {
-  curl $ZEROHEIGHT_TOKENS_URL -o properties/$ZEROHEIGHT_TOKENS_FILENAME
-}
-downloadtokens_hook
